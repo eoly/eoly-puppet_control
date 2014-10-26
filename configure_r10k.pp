0ba0269fe1c98d28f07016e44e8cc798a35fae96
Eric Olsen <eric@ericolsen.net>
2014-10-26 11:17:24 -0400

    Config directory environments and r10k.

---
+++ configure_r10k.pp
@@ -0,0 +1,16 @@
+######         ######
+##  Configure R10k ##
+######         ######
+
+class { 'r10k':
+  version           => '1.3.4',
+  sources           => {
+    'puppet' => {
+      'remote'  => 'git://github.com/eoly/eoly-puppet_control.git',
+      'basedir' => "${::settings::confdir}/environments",
+      'prefix'  => false,
+    }
+  },
+  purgedirs         => ["${::settings::confdir}/environments"],
+  manage_modulepath => false,
+}
