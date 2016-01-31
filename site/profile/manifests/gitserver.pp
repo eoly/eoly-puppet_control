2ec730ca56dd00dc767d94aa6b316c3766d020ef
Eric Olsen <eric@ericolsen.net>
2016-01-30 21:01:19 -0500

    added gitserver profile

---
+++ site/profile/manifests/gitserver.pp
@@ -0,0 +1,22 @@
+class profile::gitserver {
+
+  $password         = hiera('profile::gitserver::password')
+  $authorized_keys  = hiera('profile::gitserver::authorized_keys')
+
+  user { 'git':
+    ensure         => present,
+    home           => '/home/git',
+    managehome     => true,
+    password       => $password,
+    purge_ssh_keys => true
+  }
+
+  $authorized_keys.each |$key, $value| {
+    ssh_authorized_key { $key:
+      user    => 'git',
+      type    => 'ssh-rsa',
+      key     => $value,
+      options => "command='git'"
+    }
+  }
+}
