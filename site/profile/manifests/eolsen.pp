9a54be8096fc240b22a4448237433d271678e62e
Eric Olsen <eric@ericolsen.net>
2016-02-20 09:07:42 -0500

    cleanup

--- site/profile/manifests/eolsen.pp
+++ site/profile/manifests/eolsen.pp
@@ -1,25 +1,3 @@
 class profile::eolsen {
 
-  $eolsen_user = hiera('eolsen_user')
-
-  validate_hash($eolsen_user)
-  validate_string($eolsen_user[password])
-  validate_array($eolsen_user[groups])
-
-  user { 'eolsen':
-    ensure     => present,
-    password   => $eolsen_user[password],
-    groups     => $eolsen_user[groups],
-    managehome => true,
-    home       => '/home/eolsen',
-  }
-
-  vcsrepo { '/home/eolsen/.vim':
-    ensure   => present,
-    provider => 'git',
-    source   => 'https://github.com/eoly/eoly-vimrc.git',
-    user     => 'eolsen',
-    require  => User['eolsen'],
-  }
-
 }
