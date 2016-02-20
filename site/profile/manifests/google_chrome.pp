c69d469534a9b55398e750ec23afdb9843e0aca9
Eric Olsen <eric@ericolsen.net>
2016-02-20 12:03:42 -0500

    change package

--- site/profile/manifests/google_chrome.pp
+++ site/profile/manifests/google_chrome.pp
@@ -2,7 +2,7 @@ class profile::google_chrome {
   $rpm_url = 'https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm'
 
   package { 'google-chrome-stable_current':
-    ensure   => present,
+    ensure   => installed,
     provider => dnf,
     source   => $rpm_url
   }
