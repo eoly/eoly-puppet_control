4e46f100efdb8b189d4730c01f2cd1f25cf8f35c
Eric Olsen <eric@ericolsen.net>
2016-02-20 11:59:58 -0500

    add provider

--- site/profile/manifests/google_chrome.pp
+++ site/profile/manifests/google_chrome.pp
@@ -2,7 +2,8 @@ class profile::google_chrome {
   $rpm_url = 'https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm'
 
   package { 'google_chrome':
-    ensure => present,
-    source => $rpm_url
+    ensure   => present,
+    provider => dnf,
+    source   => $rpm_url
   }
 }
