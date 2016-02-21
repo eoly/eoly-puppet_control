cdcfab89f281629f2a66aa82033e63c491c0b93a
Eric Olsen <eric@ericolsen.net>
2016-02-21 11:12:18 -0500

    added dotfiles profile

---
+++ site/profile/manifests/dotfiles.pp
@@ -0,0 +1,28 @@
+class profile::dotfiles (
+  $user,
+  $repo = 'https://github.com/eoly/dotfiles.git'
+){
+
+  vcsrepo { "/home/${user}/dotfiles":
+    ensure   => present,
+    provider => git,
+    source   => $repo
+  }
+
+  file { "/home/${user}/.vimrc.after":
+    ensure  => link,
+    owner   => $user,
+    group   => $user,
+    target  => "/home/${user}/dotfiles/vimrc.after",
+    require => Vcsrepo["/home/${user}/dotfiles"]
+  }
+
+  file { "/home/${user}/.vimrc.bundles":
+    ensure  => link,
+    owner   => $user,
+    group   => $user,
+    target  => "/home/${user}/dotfiles/vimrc.bundles",
+    require => Vcsrepo["/home/${user}/dotfiles"]
+  }
+
+}
