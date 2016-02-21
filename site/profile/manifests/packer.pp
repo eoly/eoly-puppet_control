3a4dd6e06985416bfb894587dedff029c1ca8644
Eric Olsen <eric@ericolsen.net>
2016-02-21 10:47:45 -0500

    set version

--- site/profile/manifests/packer.pp
+++ site/profile/manifests/packer.pp
@@ -1,6 +1,8 @@
 class profile::packer {
+  $version = '0.8.6'
+
   class { 'packer':
-    version  => '0.8.6',
-    base_url => 'https://releases.hashicorp.com/packer'
+    version  => $version,
+    base_url => "https://releases.hashicorp.com/packer/${version}/"
   }
 }
