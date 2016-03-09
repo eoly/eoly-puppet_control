ee748a0279c9bc63f62bae08e448b1fd10b0eb1a
Eric Olsen <eric@ericolsen.net>
2016-03-09 11:47:46 -0500

    switch back to ensure present

--- site/profile/manifests/dotvim.pp
+++ site/profile/manifests/dotvim.pp
@@ -4,7 +4,7 @@ class profile::dotvim (
 ) {
 
   vcsrepo { "/home/${user}/dotvim":
-    ensure   => latest,
+    ensure   => present,
     provider => git,
     source   => $repo,
     user     => $user
