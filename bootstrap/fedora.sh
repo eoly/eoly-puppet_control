e9705661c88994cc02f8f4bdbbdf7473cb0637e2
Eric Olsen <eric@ericolsen.net>
2016-03-29 10:11:59 -0400

    created bootstrap/fedora.sh

---
+++ bootstrap/fedora.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install puppetlabs repo
+sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-fedora-22.noarch.rpm
+
+# install puppet-agent
+sudo dnf install puppet-agent
+
+# install r10k module
+sudo /opt/puppetlabs/bin/puppet module install zack-r10k
