284c1d5ffbf5e1b804cc083b0ed74d1f52849f17
Eric Olsen <eric@ericolsen.net>
2016-04-10 20:26:50 -0400

    update postfix profile to support virtual mailboxes

--- site/profile/manifests/postfix.pp
+++ site/profile/manifests/postfix.pp
@@ -1,28 +1,40 @@
 class profile::postfix {
 
-  $myhostname             = hiera('postfix_myhostname')
-  $mydomain               = hiera('postfix_mydomain')
-  $mydestination          = hiera('postfix_mydestination')
-  $inet_interfaces        = hiera('postfix_inet_interfaces')
-  $mail_name              = hiera('postfix_mail_name')
-  $mynetworks             = hiera('postfix_mynetworks')
-  $virtual_alias_domains  = hiera('postfix_virtual_alias_domains')
-  $virtual_alias_maps     = hiera('postfix_virtual_alias_maps')
-  $postfix_virtual        = hiera('postfix_virtual')
+  $myhostname                 = hiera('postfix_myhostname')
+  $mydomain                   = hiera('postfix_mydomain')
+  $mydestination              = hiera('postfix_mydestination')
+  $inet_interfaces            = hiera('postfix_inet_interfaces')
+  $mail_name                  = hiera('postfix_mail_name')
+  $mynetworks                 = hiera('postfix_mynetworks')
+  $virtual_mailbox_domains    = hiera('postfix_virtual_mailbox_domains')
+  $virtual_mailbox_maps       = hiera('postfix_virtual_alias_maps')
+  $postfix_virtual_mailboxes  = hiera('postfix_virtual_aliases')
+  $virtual_alias_domains      = hiera('postfix_virtual_alias_domains')
+  $virtual_alias_maps         = hiera('postfix_virtual_alias_maps')
+  $postfix_virtual_aliases    = hiera('postfix_virtual_aliases')
 
   class { '::postfix::server':
-    myhostname            => $myhostname,
-    mydomain              => $mydomain,
-    mydestination         => $mydestination,
-    inet_interfaces       => $inet_interfaces,
-    mail_name             => $mail_name,
-    mynetworks            => $mynetworks,
-    virtual_alias_domains => $virtual_alias_domains,
-    virtual_alias_maps    => $virtual_alias_maps,
+    myhostname              => $myhostname,
+    mydomain                => $mydomain,
+    mydestination           => $mydestination,
+    inet_interfaces         => $inet_interfaces,
+    mail_name               => $mail_name,
+    mynetworks              => $mynetworks,
+    virtual_mailbox_domains => $virtual_mailbox_domains,
+    virtual_mailbox_maps    => $virtual_mailbox_maps,
+    virtual_mailbox_base    => '/var/spool/mail',
+    virtual_uid_maps        => 'static:5000',
+    virtual_gid_maps        => 'static:5000',
+    virtual_alias_domains   => $virtual_alias_domains,
+    virtual_alias_maps      => $virtual_alias_maps,
   }
 
   postfix::dbfile { 'virtual':
-    content => template('profile/postfix_virtual.erb'),
+    content => template('profile/postfix_virtual_aliases.erb'),
+  }
+
+  postfix::dbfile { 'vmailbox':
+    content => template('profile/postfix_virtual_mailboxes.erb'),
   }
   
 }
